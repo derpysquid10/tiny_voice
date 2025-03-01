3e63b0effc047786c9114d226352ff7bd5675bf4
GabrielWu2004 <gabrielwu2004@gmail.com>
2025-02-28 22:30:32 -0500

    data preprocessing

--- data/load_data.py
+++ data/load_data.py
@@ -3,15 +3,19 @@ Download Afrispeech-200 data from huggingface datasets
 """
 
 
-from datasets import load_dataset
+from datasets import load_dataset, DatasetDict
 
 if __name__ == "__main__":
-    afrispeech = load_dataset("tobiolatunji/afrispeech-200", "isizulu", split="train", cache_dir="data/cache")
+    afrispeech = DatasetDict()
+    afrispeech["train"] = load_dataset("tobiolatunji/afrispeech-200", "isizulu", split="train", cache_dir="data/cache")
+    afrispeech["val"] = load_dataset("tobiolatunji/afrispeech-200", "isizulu", split="validation", cache_dir="data/cache")
+    afrispeech["test"] = load_dataset("tobiolatunji/afrispeech-200", "isizulu", split="test", cache_dir="data/cache")
     print("Afrispeech-200 dataset loaded successfully!")
 
     # look at the data
-    print("Number of samples: ", len(afrispeech))
-    print("First sample metadata: ")
-    print(afrispeech[0])
+    print("Number of training samples: ", len(afrispeech["train"]))
+    print("Number of validation samples: ", len(afrispeech["val"]))
+    print("Number of test samples: ", len(afrispeech["test"]))
+    print("Dataset info: ", afrispeech)
     print("cache file location:")
     print(afrispeech.cache_files)
